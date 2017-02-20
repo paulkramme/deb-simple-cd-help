b04c7c10c6917edee53a34d41e25972e2b232c4d
Paul Kramme <pjkramme@gmail.com>
2017-02-20 21:23:44 +0100

    Add dscihelp

--- dscihelp.py
+++ dscihelp.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python3
 
+# ARGS:  path_to_apikey
+
 import sys
 
 def main():
 	print("DEB-SIMPLE CI HELPER")
-	path = sys.argv[1]
+	
+	apipath = sys.argv[2]
+	apitoken = ""
 	with open(path, "r") as f:
 		apitoken = f.read()
-		apitoken = apitoken.rstrip()
-		print(apitoken)
+	apitoken = apitoken.rstrip()
+
+	url = sys.argv[1]
+	url.replace("API_PLACEHOLDER", apitoken)
+	os.system(url)
 
 if __name__ == "__main__":
 	main()
