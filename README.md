b26feea8d689029d41840b5d28614a37e1651d14
Paul Kramme <pjkramme@gmail.com>
2017-02-20 21:36:58 +0100

    Add usage

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # deb-simple-ci-help
 Helper script for deb-simples API token
+
+## Usage
+`dscdhelp.py <url> <path to api token file>`  
+
+Replace the token in the URL with API_TOKEN. The program
+will replace API_TOKEN with the token specified in the api file.
+This tool is necessary if you want to Continuous Deliver your build 
+packages to deb-simple server, without having to expose your API token, 
+as the token will lie at a not accessible place at your build server.  
+
+I recommend including the file into any CD project.
+
--- dscdhelp.py
+++ dscdhelp.py
@@ -3,10 +3,17 @@
 # ARGS:  path_to_apikey
 
 import sys
+import os
 
-def main():
-	print("DEB-SIMPLE CI HELPER")
-	
+def main():	
+	if len(sys.argv) < 3:
+		print("""USAGE: url path-to-api-file
+	Add a file to a secure location on your CD system.
+	In this file there should only be the api token, NOTHING ELSE.
+	Replace the API token in the URL with API_TOKEN.
+	This program will replace the API token with the real one.""")
+		sys.exit()
+
 	apipath = sys.argv[2]
 	apitoken = ""
 	with open(path, "r") as f:
@@ -14,7 +21,7 @@ def main():
 	apitoken = apitoken.rstrip()
 
 	url = sys.argv[1]
-	url.replace("API_PLACEHOLDER", apitoken)
+	url.replace("API_TOKEN", apitoken)
 	os.system(url)
 
 if __name__ == "__main__":
